d60d5be8458a7878fd82f1430fa5724cab42a7fb
b34nb0 <fbhirakata@gmail.com>
2019-09-15 20:11:41 -0500

    Extend Calculator class to add operations power, log, sin and cos

--- calculator.js
+++ calculator.js
@@ -17,7 +17,7 @@ class Calculator {
   }
 
 	initVariables() {
-		// Inicializa variables
+		// Initialise variables
 		this.number_one = 0;
     this.number_two = 0;
     this.result = 0;
@@ -25,12 +25,12 @@ class Calculator {
 	}
 
   initScreen() {
-    // Colocar 0 en la pantalla
+    // Print 0 on screen
     this.screen.innerText = "0";
   }
 
   initButtonEvents() {
-    // Asignar eventos a los botones
+    // Assign click events to buttons
     for (let i = 0; i < this.buttons.length; i++) {
 
       if (this.buttons[i].classList.contains("number")) {
@@ -45,7 +45,7 @@ class Calculator {
       } else if (this.buttons[i].classList.contains("operator")) {
         this.buttons[i].addEventListener("click", this.storeFirstNumberAndOperator.bind(this));
 
-      } else if (this.buttons[i].classList.contains("equal")) {
+      } else if (this.buttons[i].classList.contains("equals")) {
         this.buttons[i].addEventListener("click", this.operateNumbers.bind(this));
       }
 
@@ -54,68 +54,184 @@ class Calculator {
   }
 
   showNumber(e) {
-    // Mostrar numero presionado en pantalla
+    // Print pressed number on screen
     let numberPressed = e.target.innerText;
     if (this.screen.innerText === "0" || this.resultOnScreen === true) {
       this.screen.innerText = numberPressed;
     } else {
-      this.screen.innerText += numberPressed;
+      if(this.screen.innerText.length < 10) {
+        this.screen.innerText += numberPressed;
+      }
     }
+
 		this.resultOnScreen = false;
 
   }
 
   showPoint() {
-    // Concatena punto decimal
+    // Concatenates decimal point
     if (!this.screen.innerText.includes(".")) {
       this.screen.innerText += ".";
+      this.resultOnScreen = false;
     }
   }
 
   storeFirstNumberAndOperator(e) {
-    // Almacena 1er numero y el operador presionado
+    // Stores number displayed on screen and operator pressed
     this.number_one = Number(this.screen.innerText);
     this.operator = e.target.innerText;
-		this.initScreen();
+		this.resultOnScreen = true;
   }
 
   operateNumbers() {
-    // Almacenar 2do numero y ejecutar la operación
-    this.number_two = Number(this.screen.innerText);
-    switch (this.operator) {
-      case "÷":
-        this.result = this.number_one / this.number_two;
-        break;
-      case "×":
-        this.result = this.number_one * this.number_two;
-        break;
-      case "+":
-        this.result = this.number_one + this.number_two;
-        break;
-      case "-":
-        this.result = this.number_one - this.number_two;
-        break;
-      default:
-				console.log("Error");
+    // Stores (second) number displayed on screen and calculates result
+    if(!this.resultOnScreen) {
+      this.number_two = Number(this.screen.innerText);
+      switch (this.operator) {
+        case "÷":
+          this.result = this.number_one / this.number_two;
+          break;
+        case "×":
+          this.result = this.number_one * this.number_two;
+          break;
+        case "+":
+          this.result = this.number_one + this.number_two;
+          break;
+        case "-":
+          this.result = this.number_one - this.number_two;
+          break;
+        default:
+  				console.log("Error. Operator: " + this.operator);
+      }
+
+      // Prints result on screen
+  		this.showResult();
+  		// Resets values of numbers, operator and result
+  		this.initVariables();
     }
 
-		// Muestra resultado en pantalla
-		this.showResult();
-		// Reinicia variables de numeros, operador y resultado
-		this.initVariables();
   }
 
 	showResult() {
-		// Muestra resultado en pantalla
-		this.screen.innerText = this.result;
+		// Prints result on screen
+		this.screen.innerText = this.result.toString().substr(0, 10);
 		this.resultOnScreen = true;
 	}
 
   resetCalculator() {
-    // Limpiar variables, limpiar pantalla y llamar initScreen()
+    // Resets variables and prints 0 on screen
 		this.initVariables();
 		this.initScreen();
   }
 }
 
-let html_calculator = new Calculator();
+
+class SciCalculator extends Calculator {
+
+  constructor() {
+    super();
+  }
+
+  initButtonEvents() {
+    for (let i = 0; i < this.buttons.length; i++) {
+      if (this.buttons[i].classList.contains("number")) {
+        this.buttons[i].addEventListener("click", this.showNumber.bind(this));
+
+      } else if (this.buttons[i].classList.contains("point")) {
+        this.buttons[i].addEventListener("click", this.showPoint.bind(this));
+
+      } else if (this.buttons[i].classList.contains("clear")) {
+        this.buttons[i].addEventListener("click", this.resetCalculator.bind(this));
+
+      } else if (this.buttons[i].classList.contains("operator")) {
+        this.buttons[i].addEventListener("click", this.storeFirstNumberAndOperator.bind(this));
+
+      } else if(this.buttons[i].classList.contains("log-btn")){
+        this.buttons[i].addEventListener("click", this.logOperator.bind(this));
+
+      } else if(this.buttons[i].classList.contains("power-btn")) {
+        this.buttons[i].addEventListener("click", this.powerOperator.bind(this));
+
+      } else if(this.buttons[i].classList.contains("sin-btn")) {
+        this.buttons[i].addEventListener("click", this.sinOperator.bind(this));
+
+      } else if(this.buttons[i].classList.contains("cos-btn")) {
+        this.buttons[i].addEventListener("click", this.cosOperator.bind(this));
+
+      } else if(this.buttons[i].classList.contains("equals")) {
+        this.buttons[i].addEventListener("click", this.sciEqualsOperator.bind(this));
+      }
+    }
+  }
+
+  logOperator(e) {
+    // Stores number displayed on screen and operator pressed
+    this.number_one = Number(this.screen.innerText);
+    this.operator = e.target.innerText;
+		this.resultOnScreen = true;
+  }
+
+  powerOperator(e) {
+    // Stores number displayed on screen and operator pressed
+    this.number_one = Number(this.screen.innerText);
+    this.operator = e.target.innerText;
+		this.resultOnScreen = true;
+  }
+
+  sinOperator() {
+    this.number_one = Number(this.screen.innerText);
+    this.result = Math.sin(this.number_one);
+		this.resultOnScreen = true;
+    // Prints result on screen
+    this.showResult();
+    // Resets values of numbers, operator and result
+    this.initVariables();
+  }
+
+  cosOperator() {
+    this.number_one = Number(this.screen.innerText);
+    this.result = Math.cos(this.number_one);
+		this.resultOnScreen = true;
+    // Prints result on screen
+    this.showResult();
+    // Resets values of numbers, operator and result
+    this.initVariables();
+  }
+
+  sciEqualsOperator() {
+    // Stores (second) number displayed on screen and calculates result
+    if(!this.resultOnScreen) {
+      this.number_two = Number(this.screen.innerText);
+      switch (this.operator) {
+        case "÷":
+          this.result = this.number_one / this.number_two;
+          break;
+        case "×":
+          this.result = this.number_one * this.number_two;
+          break;
+        case "+":
+          this.result = this.number_one + this.number_two;
+          break;
+        case "-":
+          this.result = this.number_one - this.number_two;
+          break;
+        case "^":
+          this.result = Math.pow(this.number_one, this.number_two);
+          break;
+        case "log":
+          this.result = Math.log(this.number_one) / Math.log(this.number_two);
+          break;
+        default:
+  				console.log("Error. Unknown operator: " + this.operator);
+      }
+
+      // Prints result on screen
+  		this.showResult();
+  		// Resets values of numbers, operator and result
+  		this.initVariables();
+    }
+  }
+
+}
+
+let html_calculator = new SciCalculator();
